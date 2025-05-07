439ccba9c0c4a87d327054e5dbf82bb67d05e5f4
O46001831 <81324184+O46001831@users.noreply.github.com>
2025-05-07 15:39:39 +0200

    Delete NewGetOpen/NewGetOpen directory
    trovato duplicato nidificato nella cartella principale

--- NewGetOpen/NewGetOpen/templates/content.php
+++
@@ -1,6 +0,0 @@
-<?php require __DIR__ . "/sections/firstSection.php" ?>  
-<?php require __DIR__ . "/sections/gallery.php" ?>  
-<?php require __DIR__ . "/sections/clientiAndPartner.php" ?>  
-<?php require __DIR__ . "/sections/servizi.php" ?>  
-<?php require __DIR__ . "/sections/files.php" ?>  
-<?php require __DIR__ . "/sections/secondSection.php" ?>  
